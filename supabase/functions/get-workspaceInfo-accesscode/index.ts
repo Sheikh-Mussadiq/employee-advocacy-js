01f1d97877429f810de994b128dcefa06b185dc1
Mukarram Nawaz <156927769+MukarramNawaz@users.noreply.github.com>
2025-04-25 17:37:54 +0500

    feat: Add get-workspaceInfo-accesscode function

---
+++ supabase/functions/get-workspaceInfo-accesscode/index.ts
@@ -0,0 +1,60 @@
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+// Environment variables
+const SUPABASE_URL = Deno.env.get('SUPABASE_URL')!
+const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
+
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'x-client-info, apikey, content-type',
+  'Content-Type': 'application/json',
+};
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+
+Deno.serve(async (req: Request) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
+  }
+
+  if (req.method !== 'POST') {
+    return new Response(JSON.stringify({ error: 'Method not allowed' }), { headers: corsHeaders, status: 405 });
+  }
+  try {
+    const { access_token } = await req.json();
+
+    if (!access_token) {
+      return new Response(
+        JSON.stringify({ error: 'Access token is required' }),
+        { headers: corsHeaders, status: 400 }
+      );
+    }
+
+    const { data, error } = await supabase
+      .from('workspace')
+      .select('id, name, description, img_path')
+      .eq('access_token', access_token)
+      .single();
+
+    if (error) {
+      return new Response(
+        JSON.stringify({ error: error.message }),
+        { headers: corsHeaders, status: 500 }
+      );
+    }
+
+    return new Response(
+      JSON.stringify(data),
+      { headers: corsHeaders, status: 200 }
+    );
+
+  } catch (_error) {
+    return new Response(
+      JSON.stringify({ error: 'Internal server error' }),
+      { headers: corsHeaders, status: 500 }
+    );
+  }
+  
+})
